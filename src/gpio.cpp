64c91d71e6df864b6298ad0bbe18a2195a2dbf10
Pawel Debski <pawel.debski.ext@gigaset.com>
2018-02-18 22:55:57 +0100

    char sent via uart

--- src/gpio.cpp
+++ src/gpio.cpp
@@ -4,14 +4,18 @@
 #include <avr/sleep.h>
 #define 	F_CPU   8000000UL
 #include <util/delay.h>
-#include <utility>
 
 int main (void)
 {
 	DDRC = 0xFF;
 	uint8_t out = 0x01;
+
+    UBRR1L = 51;
+    UCSR1B |= 1<<3;
+
     while(true)
     {
+        UDR1 = 'o';
         PORTC = ~out;
         _delay_ms(1000);
         out <<= 1;
