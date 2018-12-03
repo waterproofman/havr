d634659c5e949d8fff42fd4af42705a89cca46e9
Pawel Debski <pawel.debski.ext@gigaset.com>
2018-12-03 23:09:08 +0100

    tx interrupt enabled

--- src/uart.cpp
+++ src/uart.cpp
@@ -2,11 +2,29 @@
 #include <avr/io.h>
 #include "uart.hpp"
 
+#include <avr/interrupt.h>
+
+
+
 namespace havr
 {
 
 namespace
 {
+    ISR(USART1_TX_vect)
+    {
+        static bool once = false;
+        if(!once)
+        {
+            UDR1 = 'i';
+            once = true;
+        }
+        else
+        {
+            once = false;
+        }
+    }
+
     void rxIsr()
     {
     }
@@ -20,6 +38,8 @@ void Uart::configure()
 {
     UBRR1L = 51;
     UCSR1B |= 1<<3;
+    UCSR1B |= 1<<6;
+    SREG |= 1<<7;
 }
 
 void Uart::send(uint8_t byte)
